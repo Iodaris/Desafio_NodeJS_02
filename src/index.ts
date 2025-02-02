7c4986fe2b4cad6a47e77b8db3aeaac75af3b4a6
Guilherme-GN <guilherme.gnascimento45@gmail.com>
2025-02-02 15:30:51 -0300

    Implementa funcionalidades:
    Soma a idade de todos os ubuntus
    Agrupa o nome de todos os ubuntus
    Calcula média IMC de todos ubuntus
    Conta quantos ubuntus são devs
    Exibe somente ubuntus com sobrenome Silva

--- src/index.ts
+++ src/index.ts
@@ -39,5 +39,55 @@ app.listen(PORT, () => {
 
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
-	
+console.log('\nDesafio 002\n');
+
+const ubuntus = [
+	{ nome: 'Carlos Silva', idade: 33, peso: 88.5, altura: 1.65, dev: false},
+  	{ nome: 'Paula Souza', idade: 40, peso: 70.3, altura: 1.75, dev: true },
+  	{ nome: 'Pedro Silva', idade: 28, peso: 65.0, altura: 1.68, dev: true },
+  	{ nome: 'Roberta Oliveira', idade: 22, peso: 80.2, altura: 1.80, dev: true },
+  	{ nome: 'Diego Silva', idade: 35, peso: 90.0, altura: 1.70, dev: false }
+];
+
+let totIdade = 0;
+for (const soma of ubuntus) {
+	totIdade += soma.idade;
+}
+console.log(`Soma das idades: ${totIdade}\n`);
+
+let nomesAgrupados = '';
+for (let i = 0; i < ubuntus.length; i++) {
+	if (i === ubuntus.length -1) {
+		nomesAgrupados += ubuntus[i].nome;
+	} else {
+		nomesAgrupados += ubuntus[i].nome + ', ';
+	}
+}
+console.log(`Nomes agrupados: ${nomesAgrupados}\n`);
+
+let mediaIMC = 0;
+for (const ubuntu of ubuntus) {
+	mediaIMC += ubuntu.peso / (ubuntu.altura ** 2);
+}
+mediaIMC /= ubuntus.length;
+console.log(`Média de IMC: ${mediaIMC.toFixed(2)}\n`);
+
+let totDev = 0;
+for (let i = 0; i < ubuntus.length; i++){
+	if (ubuntus[i].dev) {
+		totDev++;
+	}
+}
+console.log(`Total de Ubuntus que são devs: ${totDev}\n`);
+
+let silva = '';
+for (let i = 0; i<ubuntus.length; i++) {
+	if (ubuntus[i].nome.includes('Silva')) {
+		if (silva) {
+			silva += ', ';
+		}
+		silva += ubuntus[i].nome;
+	}
+}
+console.log(`Nomes com sobrenome Silva: ${silva}`);
 });
